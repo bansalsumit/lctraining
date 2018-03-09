fe1591d01754a02acc6abc627fcc49892db4b65b
bansalsumit <sumit.bansal@babatrucks.com>
2018-03-09 10:51:27 +0530

    method and local parameter name changes

--- ruby/occurrence_hash/lib/occurrence_hash.rb
+++ ruby/occurrence_hash/lib/occurrence_hash.rb
@@ -1,7 +1,7 @@
 class CharOccurrenceChecker
-  def char_occurrence(incoming_data)
-    output_hash = Hash.new(0)
-    incoming_data.each { |char| output_hash[char] += 1 }
-    output_hash
+  def alphabet_repeat(incoming_string)
+    repeatness_of_alphabet = Hash.new(0)
+    incoming_string.each { |char| repeatness_of_alphabet[char] += 1 }
+    repeatness_of_alphabet
   end
 end
