343b538a533380e884c2f1c37fa78df351fcf6cf
bansalsumit <sumit.bansal@babatrucks.com>
2018-03-09 13:04:21 +0530

    move logic to one place

--- ruby/highlight_search_result/main.rb
+++ ruby/highlight_search_result/main.rb
@@ -2,13 +2,15 @@
 # string and highlights the searched element. Also it should display the
 # total no of searches found.
 
-require_relative "lib/highlight_search_result.rb"
+require_relative "lib/highlight_search_result"
 
 puts "enter string"
 incoming_string = gets.chop
+
 puts "search element"
 search_element = gets.chop
 
 string_modifier = StringModifier.new
+
 puts string_modifier.search_element(search_element, incoming_string)
-puts string_modifier.total_search_found(search_element, incoming_string.split(" "))
+puts string_modifier.total_search_found(search_element, incoming_string)
