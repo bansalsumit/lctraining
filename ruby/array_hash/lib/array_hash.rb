363e02dbd7b4bd8f00c701c34ffdf44032f96827
bansalsumit <sumit.bansal@babatrucks.com>
2018-03-09 12:52:52 +0530

    change logic to one place and method name change

--- ruby/array_hash/lib/array_hash.rb
+++ ruby/array_hash/lib/array_hash.rb
@@ -1,9 +1,7 @@
 class List
 
-  def element_by_length(input_list)
-    input_list.each.group_by{ |element|
-      element.length
-    }
+  def element_by_length_and_sort_by_key(input_list)
+    input_list.each.group_by { |element| element.length }.sort.to_h
   end
 
 end
--- ruby/array_hash/main.rb
+++ ruby/array_hash/main.rb
@@ -1,9 +1,9 @@
-#Create a method for Array that returns a hash having 'key' as length of
-#the element and value as an array of all the elements of that length.
-#Make use of Array#each.
-#Returned Hash should be sorted by key.
+# Create a method for Array that returns a hash having 'key' as length of
+# the element and value as an array of all the elements of that length.
+# Make use of Array#each.
+# Returned Hash should be sorted by key.
 
-require_relative "lib/array_hash.rb"
+require_relative "lib/array_hash"
 
 list = List.new
-p list.element_by_length(gets.chop.split(" ")).sort.to_h
+p list.element_by_length_and_sort_by_key(gets.chop.split(" "))
